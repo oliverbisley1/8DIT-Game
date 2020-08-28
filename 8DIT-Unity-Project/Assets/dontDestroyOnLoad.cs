bfbca913a2a04dfd93b813441a9207f3f2cf4265
oliverbisley1 <16obisley@wakatipu.school.nz>
2020-08-28 20:15:16 +1200

    sounds
    sounds

---
+++ 8DIT-Unity-Project/Assets/dontDestroyOnLoad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dontDestroyOnLoad : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] audioClipArray;
+
+    void Start()
+    { 
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    AudioClip RandomClip()
+    {
+        return audioClipArray[Random.Range(0, audioClipArray.Length - 1)];
+    }
+}
---
+++ 8DIT-Unity-Project/Assets/loicSounds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loicSounds : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] audioClipArray;
+
+    void Start()
+    {
+        audioSource.PlayOneShot(RandomClip());
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    AudioClip RandomClip()
+    {
+        return audioClipArray[Random.Range(0, audioClipArray.Length - 1)];
+    }
+}
